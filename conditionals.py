5c9dd70e361e7bb0eaa4ff4b11d50c0ca91de5e5
Anki <ankita38gupta38@gmail.com>
2023-06-10 23:32:05 +0530

    first commit

---
+++ conditionals.py
@@ -0,0 +1,9 @@
+import time 
+timetamp=time.strftime('%H: %M: %S:')
+print(timetamp)
+timetamp=time.strftime('%H')
+print(timetamp)
+timetamp=time.strftime('%M')
+print(timetamp)
+timetamp=time.strftime('%S')
+print(timetamp)
